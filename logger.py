cae9db5e0098a19d8de26261c0d22bb4fa8b52ec
ibuda <ivan.budanaev@gmail.com>
2020-07-05 11:10:52 +0300

    logs module

--- logger.py
+++ logger.py
@@ -7,7 +7,7 @@ PROJ_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/'
 
 def logger(name):
     logging.config.fileConfig(f'{PROJ_FOLDER}logger.conf')
-    logging.getLogger('boxsdk.network.default_network').setLevel(logging.ERROR)
+    logging.getLogger('apscheduler.executors.default').setLevel(logging.ERROR)
     LOGGER = logging.getLogger(name)
 
     return LOGGER
